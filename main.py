8b9186329d2bccebef265c87169a8db753ae3079
georgii <mygdshowcases@gmail.com>
2025-08-25 20:24:41 +0200

    Added messages of wrong password on admin page. Added ability to handle error 404, now it sends you to home page.

--- main.py
+++ main.py
@@ -31,7 +31,13 @@ with sqlite3.connect('database.db') as database:
         cursor.execute('INSERT INTO main(name, score) VALUES("11D", 0);')
         cursor.execute('INSERT INTO main(name, score) VALUES("11E", 0);')
         cursor.execute('INSERT INTO main(name, score) VALUES("11F", 0);')
+    database.commit()
 
+
+def checkPassword(password):
+    if passwordHash==hashlib.sha256(password.encode("utf-8")).hexdigest():
+        return True
+    return False
 
 @app.route('/')
 def home():
@@ -42,30 +48,31 @@ def home():
         ''')
         scores=cursor.fetchall()
         scores=[score[0] for score in scores]
-        print(scores)
+        #print(scores)
     return render_template('main.html', scores=scores)
 
-
-
-@app.route('/admin', methods=['GET'])
+@app.route('/admin/')
 def admin():
-    if request.method=='GET':
-        with sqlite3.connect('database.db') as database:
-            cursor = database.cursor()
-            cursor.execute('''
-            SELECT score FROM main;
-            ''')
-            scores = cursor.fetchall()
-        return render_template('admin.html', scores=scores)
-    return 0
+    #if request.method=='GET':
+    with sqlite3.connect('database.db') as database:
+        cursor = database.cursor()
+        cursor.execute('''
+        SELECT score FROM main;
+        ''')
+        scores = cursor.fetchall()
+    return render_template('admin.html', scores=scores)
 
-@app.route('/add/<hmRm>/<amount>//<pas>', methods=['GET', 'POST'])
+
+@app.route('/add/<hmRm>/<amount>/<pas>', methods=['GET', 'POST'])
 def add(hmRm, amount, pas):
     #rint(pas)
     #print(passwordHash)
     #print(hashlib.sha256(pas.encode("utf-8")).hexdigest())
-    if passwordHash!=hashlib.sha256(pas.encode("utf-8")).hexdigest():
+    print('add')
+    if not checkPassword(pas):
+        print('add redirected home')
         return redirect(url_for("home"))
+
 
     with sqlite3.connect('database.db') as database:
         cursor = database.cursor()
@@ -75,9 +82,17 @@ def add(hmRm, amount, pas):
         cursor.execute(f'''
         INSERT INTO logs(classId, amount) VALUES({hmRm}, {amount})
         ''')
+        database.commit()
 
     return redirect(url_for("admin"))
 
+@app.route('/checkPassword/<password>')
+def anotherCheckPass(password):
+    print(str(checkPassword(password)))
+    return str(checkPassword(password))
 
+@app.errorhandler(404)
+def pageNotFound(e):
+    return redirect(url_for('home'))
 
 app.run()
--- static/admin.js
+++ static/admin.js
@@ -1,3 +1,9 @@
+//var xmlHttp = new XMLHttpRequest();
+//xmlHttp.open( "GET", "/checkPassword/yo", false );
+//xmlHttp.send( null );
+//console.log(xmlHttp.responseText)
+
+
 function getCookie(name) {
     const cookieList = document.cookie.split("; ")
     for (const cookie of cookieList) {
@@ -8,37 +14,59 @@ function getCookie(name) {
 }
 
 function addCookie(name, value) {
+
     document.cookie = name+"="+value+"; path=/"
 }
 
-
-
-
-if (getCookie("value")!=null){
-inp=document.getElementById("inp")
-inp.value=Number(getCookie("value"))
+function sendRequest(url){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    //console.log(xmlHttp.responseText)
+    //console.log(url)
+    return xmlHttp.responseText;
 }
-
-if (getCookie("password")!=null){
-pas=document.getElementById("password")
-pas.value=getCookie("password")
-}
-
 
 function updateScore(homeroom, addition){
    form=document.getElementById("form")
    amount=document.getElementById("inp").value
    pas=document.getElementById("password").value
-   if(addition==true){
-        form.action="/add/"+homeroom+"/"+amount+"/"+pas}
+   //console.log(pas)
+   //console.log(pas.length)
+   //console.log("wtfwtfwtf1")
+   if (pas.length!=0){
+        //console.log("wtfwtfwtf2")
+        if(sendRequest("/checkPassword/"+pas)=="True"){
+            //console.log("wtfwtfwtf3")
+             if(addition==true){
+                 form.action="/add/"+homeroom+"/"+amount+"/"+pas}
+             else{
+                 form.action="/add/"+homeroom+"/"+(-amount)+"/"+pas}
+             //console.log(form.action, 'passed')
+             //console.log("/add/"+homeroom+"/"+amount+"/"+addition)
+             //addCookie("value", amount)
+             //addCookie("password", pas)
+             localStorage.setItem("value", amount)
+             localStorage.setItem("password", pas)
+             form.submit()}
+        else{
+           alert("Wrong password")}
+        //alert('second alert')}
    else{
-        form.action="/add/"+homeroom+"/"+(-amount)+"/"+pas
-   }
-   //console.log(form.action)
-   //console.log("/add/"+homeroom+"/"+amount+"/"+addition)
-   addCookie("value", amount)
-   addCookie("password", pas)
-   form.submit()
+        alert("You need to enter a password")}
+   //alert('forth alert')
+    }
 }
 
 
+
+//sendRequest("/checkPassword/"+'yo')
+function onLoad(){
+    inp=document.getElementById("inp")
+    inp.value=Number(localStorage.getItem("value"))
+    pas=document.getElementById("password")
+    pas.value=localStorage.getItem("password")
+}
+
+
+//console.log('acjfghjidfsuzgh')
